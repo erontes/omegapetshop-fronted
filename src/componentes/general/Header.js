888f1882e0fbef22c766706cee457d783b10fbe3
erontes <erontes@gmail.com>
2022-11-04 00:46:42 -0500

    hr

--- src/componentes/general/Header.js
+++ src/componentes/general/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
                 <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
                     <strong style={{ color: 'blue' }}>Omega</strong><strong style={{ color: 'black' }}>PetShop</strong>
                 </a>
-
+                <hr></hr>
                 <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="/" class="nav-link px-2 link-secondary">Inicio</a></li>
                     <li><a href="/categorias" class="nav-link px-2 link-dark">Categorías</a></li>
